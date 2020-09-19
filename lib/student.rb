98af7fc4f1ad7bc798aa82c1f30fb50453e6bc2a
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-19 20:59:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def save 
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES(?, ?)
@@ -39,5 +39,5 @@ class Student
 
 
 
-  
+
 end
