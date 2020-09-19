8348627667cf37e370c1faaac3ba6397cb800dd6
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-19 20:45:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
- 
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   def initialize(name)
    @name = name
   end
